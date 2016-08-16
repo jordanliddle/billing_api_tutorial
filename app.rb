8a940f94b73d5408ad30abf0d46fbcb3d9f665fe
Jordan Liddle <jordan.liddle@shopify.com>
2016-08-16 12:58:17 -0400

    add billing ability

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class GiftBasket < Sinatra::Base
     Dotenv.load
     @key = ENV['API_KEY']
     @secret = ENV['API_SECRET']
-    @app_url = "jamied.ngrok.io"
+    @app_url = "jordo.ngrok.io"
     @tokens = {}
     super
   end
@@ -58,30 +58,30 @@ class GiftBasket < Sinatra::Base
       else
         return [500, "Something went wrong."]
       end
+
+      # now that we have the token, we can instantiate a session
+      session = ShopifyAPI::Session.new(shop, @tokens[shop])
+      ShopifyAPI::Base.activate_session(session)
     end
 
-    # now that we have the token, we can instantiate a session
-    session = ShopifyAPI::Session.new(shop, @tokens[shop])
-    ShopifyAPI::Base.activate_session(session)
+    # now that the session is activated, we can create a recurring application charge
+    create_recurring_application_charge
 
-    # create webhook for order creation if it doesn't exist
-    if not ShopifyAPI::Webhook.find(:all).any?
-      webhook = {
-        topic: 'orders/create',
-        address: "https://#{@app_url}/giftbasket/webhook/order_create",
-        format: 'json'}
-
-      ShopifyAPI::Webhook.create(webhook)
-    end
-
-    # redirect the merchant to the bulk editor
-    bulk_edit_url = "https://www.shopify.com/admin/bulk"\
-                    "?resource_name=ProductVariant"\
-                    "&edit=metafields.test.ingredients:string"
-
+    # we want to redirect to the bulk edit URL if there is a token and an activated session
     redirect bulk_edit_url
-
   end
+
+  get '/activatecharge' do
+    # store the charge_id from the request
+    charge_id  = request.params['charge_id']
+    recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.find(charge_id)
+    recurring_application_charge.status == "accepted" ? recurring_application_charge.activate : "Please accept the charge"
+
+    # once the charge is activated, we can subscribe to the order/create webhook and redirect the user back to the bulk edit URL
+    create_order_webhook
+    redirect bulk_edit_url
+  end
+
 
   helpers do
     def verify_webhook(hmac, data)
@@ -89,6 +89,51 @@ class GiftBasket < Sinatra::Base
       calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, @secret, data)).strip
 
       hmac == calculated_hmac
+    end
+
+    def create_recurring_application_charge
+      # checks to see if there is already an RecurringApplicationCharge created and activated
+      if not ShopifyAPI::RecurringApplicationCharge.current
+        @recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new(
+                name: "Gift Basket Plan",
+                price: 4.99,
+                return_url: "https:\/\/jordo.ngrok.io\/activatecharge",
+                test: true,
+                capped_amount: 100,
+                terms: "$1 for every order created")
+
+        # if the new RecurringApplicationCharge saves, we redirect to the confirmation URL
+        if @recurring_application_charge.save
+            redirect @recurring_application_charge.confirmation_url
+        end
+      end
+    end
+
+    def bulk_edit_url
+      bulk_edit_url = "https://www.shopify.com/admin/bulk"\
+                    "?resource_name=ProductVariant"\
+                    "&edit=metafields.test.ingredients:string"
+      return bulk_edit_url
+    end
+
+    def create_order_webhook
+      # create webhook for order creation if it doesn't exist
+      if not ShopifyAPI::Webhook.find(:all).any?
+        webhook = {
+          topic: 'orders/create',
+          address: "https://#{@app_url}/giftbasket/webhook/order_create",
+          format: 'json'}
+
+        ShopifyAPI::Webhook.create(webhook)
+      end
+    end
+
+    def create_usage_charge
+      usage_charge = ShopifyAPI::UsageCharge.new(description: "1 dollar per order plan", price: 1.0)
+      recurring_application_charge_id = ShopifyAPI::RecurringApplicationCharge.last
+      usage_charge.prefix_options = {recurring_application_charge_id: recurring_application_charge_id.id}
+      usage_charge.save
+      puts "Usage charge created successfully!"
     end
   end
 
@@ -113,6 +158,9 @@ class GiftBasket < Sinatra::Base
     else
       return [403, "You're not authorized to perform this action."]
     end
+
+    # charge fee with the UsageCharge endpoint
+    create_usage_charge
 
     # parse the request body as JSON data
     json_data = JSON.parse data
